47d23ff63b754e7156ba11c452a38e605d3d0c94
Kellie Farmer <kellie.farmer@knights.ucf.edu>
2019-11-02 11:12:55 -0400

    initial commit

---
+++ farmer_p1/src/DuplicateRemover.java
@@ -0,0 +1,14 @@
+
+import java.io.IOException;
+
+public class DuplicateRemover {
+
+
+    public void remove(String dataFile) throws IOException{
+
+    }
+    public void write(String outputFile) throws IOException {
+
+    }
+
+}
---
+++ farmer_p2/src/DuplicateCounter.java
@@ -0,0 +1,13 @@
+
+import java.io.IOException;
+
+public class DuplicateCounter {
+
+    public void count(String dataFile)throws IOException{
+
+    }
+    public void write(String outputFile)throws IOException{
+
+    }
+
+}
